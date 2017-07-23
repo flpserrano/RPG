93592623aa22ba7665d73a6ab5a514bef505ccbe
flpserrano <felipeserrano6@gmail.com>
2017-07-22 18:27:00 -0600

    First upload

---
+++ GameRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Player : Entity {
+	public string[] Inventory;
+	public string[] Skills;
+	public int Money;
+}
